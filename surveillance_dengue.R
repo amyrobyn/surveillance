1bbf8737cb846cd8008f384007a4acbaba298dda
amyrobyn <amykrystosik@gmail.com>
2016-11-13 20:15:56 -0800

    mas reciente

--- surveillance_dengue.R
+++ surveillance_dengue.R
@@ -33,7 +33,7 @@ library(maptools)
 
 ###################################################create spatialpolygon from barrios shapefile###################################################
 crswgs84=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
-barrios<-readShapePoly("C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/arcgis analysis/gwr models/output/surveillance/barrios.shp",proj4string=crswgs84,verbose=TRUE)
+barrios<-readShapePoly("barrios.shp",proj4string=crswgs84,verbose=TRUE)
 class(barrios)
 barrios$ID_BARRIO<- as.numeric(as.character(barrios$ID_BARRIO))
 str(barrios@data)
@@ -59,6 +59,10 @@ counts_numeric$idx<-as.Date(counts_numeric$date_formated)
 ts(counts_numeric[2:52], start = c(2014, 10), frequency = 12)
 structure(counts_numeric)
 counts_numericsts<-sts(epoch=as.numeric(counts_numeric$idx),observed=matrix(counts_numeric$dengue),epochAsDate=TRUE, freq =12, start=c(2014, 10), map=barrios, neighbourhood = barrios_order)
+####el errror sale aqui!###
+
+
+
 str(counts_numericsts)
 class(counts_numericsts)
 plot(counts_numericsts, type=observed~time)
