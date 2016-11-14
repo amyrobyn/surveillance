fa44cf129ff118ba040752900bc94970c469f4e0
amyrobyn <amykrystosik@gmail.com>
2016-11-13 20:30:30 -0800

    note out code

--- surveillance_dengue.R
+++ surveillance_dengue.R
@@ -6,30 +6,16 @@ setwd ("C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/arcg
 pkg = c("sp", "xts", "zoo", "spacetime", "trajectories",
 "maptools", "maps", "mapview", "leaflet", "rglwidget",
 "rgl", "RColorBrewer", "ggmap", "ggplot2", "dplyr",
-"rmarkdown", "units")
+"rmarkdown", "units", "rgdal", "rgeos", "tmap", "leaflet", "spatstat", "gstat", "spdep",
+"rgdal")
 install.packages(pkg)
 
 install.packages("surveillance")
 library(surveillance)
 help(surveillance)
 help(sp)
-install.packages("zoo")
-install.packages("xts")
-install.packages("sp")
-install.packages("ggplot2")  
-install.packages("rgdal") 
-install.packages("rgeos") 
-install.packages("tmap") 
-install.packages("ggmap") 
-install.packages("leaflet") 
-install.packages("spatstat") 
-install.packages("gstat") 
-install.packages("spdep")
-install.packages("rgdal")
+
 require("ISOweek")
-library("rgdal")
-library(xts)
-install.packages("maptools")
 library(maptools)
 
 ###################################################create spatialpolygon from barrios shapefile###################################################
@@ -40,7 +26,6 @@ barrios$ID_BARRIO<- as.numeric(as.character(barrios$ID_BARRIO))
 str(barrios@data)
 plot(barrios)
 barrios$barrios_order <- nbOrder(poly2adjmat(barrios), maxlag=10)
-
 #sucess!
 
 ###################put sptialpolygon into dataframe: ##################
@@ -57,8 +42,10 @@ polygons <- SpatialPolygonsDataFrame(polygons, counts_numeric)
 ###################import numeric data- counts of dengue##################
 counts_numeric<-read.csv("C:\\Users\\amykr\\Google Drive\\Kent\\james\\dissertation\\chkv and dengue\\arcgis analysis\\gwr models\\output\\counts.csv")
 counts_numeric$idx<-as.Date(counts_numeric$date_formated)
+#create ts data class
 ts(counts_numeric[2:52], start = c(2014, 10), frequency = 12)
 structure(counts_numeric)
+#create sts data class
 counts_numericsts<-sts(epoch=as.numeric(counts_numeric$idx),observed=matrix(counts_numeric$dengue),epochAsDate=TRUE, freq =12, start=c(2014, 10), map=barrios, neighbourhood = barrios_order)
 ####el errror sale aqui!###
 
