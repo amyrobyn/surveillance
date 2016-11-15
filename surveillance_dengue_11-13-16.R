162e8d53589205586e77dfc4c76a95f1974d882d
amyrobyn <amykrystosik@gmail.com>
2016-11-14 20:27:30 -0800

    error in line 99
    dengue_fit_nepop <- update(dengueFit_offset, ne = list(f= ~log(pop)),
    data=list(pop=population(counts_sts)))
    Error in nlminb(start, negll, gradient = negsc, hessian = fi, ..., scale
    = scale,  :
    NA/NaN gradient evaluation

--- surveillance_dengue_11-13-16.R
+++ surveillance_dengue_11-13-16.R
@@ -3,7 +3,7 @@ data(measlesWeserEms)
 setwd ("C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/arcgis analysis/gwr models/output/surveillance")
 
 #install.packages("animation")
-install.packages("surveillance")
+#install.packages("surveillance")
 #install.packages("gridExtra")
 library(xts)
 library(maptools)
@@ -29,7 +29,8 @@ counts1<-counts_numeric2 %>% select(c(54,2,4)) %>% spread(key=ID,value=dengue)
 
 population<-counts_numeric2 %>% select(c(54,2,10)) %>% spread(key=ID,value=total_pop)
 population<-data.matrix(population, rownames.force = NA)
-
+population<-population/2369829
+#population cali = 2369829 in 2015 http://www.cali.gov.co/publicaciones/poblacion_de_cali_aumenta_anualmente_en_habitantes_pub
 summary(population)
 counts_sts<-sts(epoch=counts1$date_numeric,observed= counts1[,2:ncol(counts1)],epochAsDate=FALSE, freq =12, start=c(2014, 10), map=barrios, neighbourhood = barrios$barrios_order, population=population[,2:ncol(population)])
 
@@ -73,45 +74,58 @@ plot(counts_sts_fit_basic, type = "fitted", units = districts2plot, hide0s = TRU
 #Sprop<-counts_numeric2 %>% select(c(54,2,23)) %>% spread(key=ID,value=Avg_rain)
 Sprop<-as.matrix(counts_numeric2 %>% select(c(54,2,23)) %>% spread(key=ID,value=Avg_rain))[,-1]
 
-Soptions <- c("unchanged", "Soffset", "Scover")
+Soptions <- c("unchanged", "Soffset", "Scovar")
 SmodelGrid <- expand.grid(end=Soptions, ar = Soptions)
 row.names(SmodelGrid) <- do.call("paste", c(SmodelGrid, list(sep ="|")))
 
-counts_sts_multivariate <-apply(X=SmodelGrid, MARGIN = 1, FUN=function(options){ 
+#update the basic model with an offset- here they used teh vaccinated population. For us, we could use incidence as this woudl estimate those susceptible...
+#they try combinations of offset and covariate here
+dengueFits_offset <-apply(X=SmodelGrid, MARGIN = 1, FUN=function(options){ 
   updatecomp <-function(comp, option) switch(option, 
   "unchanged" = list(),
   "Soffset"=list(offset = comp$offset * Sprop), 
-"Scovar"=list(f=update(comp$f,~.+log(Sprop))))
+  "Scovar"=list(f=update(comp$f,~.+log(Sprop))))
 update(counts_sts_fit_basic, 
        end = updatecomp(counts_sts_fit_basic$control$end, options[1]),
        ar = updatecomp(counts_sts_fit_basic$control$ar, options[2]),
        data = list(Sprop = Sprop))
   })
-
+#compare the AIC from each optpion for offscet/covariate
 aics<-do.call(AIC, lapply(names(counts_sts_multivariate), as.name), envir = as.environment(counts_sts_multivariate))
-#error happens here#
+dengueFit_offset<-dengueFits_offset[["Scovar|unchanged"]]
+coef(dengueFit_offset, se = TRUE)["end.log(Sprop)",]
+
+#add spatial intercation weighted according to population fraction
+dengue_fit_nepop <- update(dengueFit_offset, ne = list(f= ~log(pop)), data=list(pop=population(counts_sts)))
+#error happens here! 
+#Error in nlminb(start, negll, gradient = negsc, hessian = fi, ..., scale = scale,  : 
+#NA/NaN gradient evaluation
+
+#weighted
+dengue_fit_powerlaw <- update(dengue_fit_nepop, ne = list(weights = W_powerlaw(maxlag = 5)))
 
 
 #other covariates I want to import
 #time and spacy varying
-rain<-counts_numeric2 %>% select(c(54,2,23)) %>% spread(key=ID,value=Avg_rain)
-rainlag1<-counts_numeric2 %>% select(c(54,2,51)) %>% spread(key=ID,value=rainlag1)
+rain<-as.matrix(counts_numeric2 %>% select(c(54,2,23)) %>% spread(key=ID,value=Avg_rain))[,-1]
+rainlag1<-as.matrix(counts_numeric2 %>% select(c(54,2,51)) %>% spread(key=ID,value=rainlag1))[,-1]
 
 #time varying
-temp<-counts_numeric2 %>% select(c(54,2,22)) %>% spread(key=ID,value=temp_anom_median_c)
-templag1<-counts_numeric2 %>% select(c(54,2,52)) %>% spread(key=ID,value=templag1)
+temp<-as.matrix(counts_numeric2 %>% select(c(54,2,22)) %>% spread(key=ID,value=temp_anom_median_c))[,-1]
+templag1<-as.matrix(counts_numeric2 %>% select(c(54,2,52)) %>% spread(key=ID,value=templag1))[,-1]
 
-str(Sprop)
+
 #fixed
-services<-counts_numeric2 %>% select(c(54,2,15)) %>% spread(key=ID,value=services_index)
-educ<-counts_numeric2 %>% select(c(54,2,11)) %>% spread(key=ID,value=assist_educ_P)
-afro<-counts_numeric2 %>% select(c(54,2,26)) %>% spread(key=ID,value=negro__a___mulato__afrop)
-area<-counts_numeric2 %>% select(c(54,2,6)) %>% spread(key=ID,value=arean3210)
-limit<-counts_numeric2 %>% select(c(54,2,12)) %>% spread(key=ID,value=alguna_limit_p)
-pop<-counts_numeric2 %>% select(c(54,2,10)) %>% spread(key=ID,value=total_pop)
-literate<-counts_numeric2 %>% select(c(54,2,13)) %>% spread(key=ID,value=literate_p)
-empty<-counts_numeric2 %>% select(c(54,2,18)) %>% spread(key=ID,value=home_empty_p)
-single<-counts_numeric2 %>% select(c(54,2,29)) %>% spread(key=ID,value=single_p)
-male<-counts_numeric2 %>% select(c(54,2,19)) %>% spread(key=ID,value=male_p)
-unemployed<-counts_numeric2 %>% select(c(54,2,27)) %>% spread(key=ID,value=unem_p)
-home<-counts_numeric2 %>% select(c(54,2,28)) %>% spread(key=ID,value=home_p)
+services<-as.matrix(counts_numeric2 %>% select(c(54,2,15)) %>% spread(key=ID,value=services_index))[,-1]
+educ<-as.matrix(counts_numeric2 %>% select(c(54,2,11)) %>% spread(key=ID,value=assist_educ_P))[,-1]
+afro<-as.matrix(counts_numeric2 %>% select(c(54,2,26)) %>% spread(key=ID,value=negro__a___mulato__afrop))[,-1]
+area<-as.matrix(counts_numeric2 %>% select(c(54,2,6)) %>% spread(key=ID,value=arean3210))[,-1]
+limit<-as.matrix(counts_numeric2 %>% select(c(54,2,12)) %>% spread(key=ID,value=alguna_limit_p))[,-1]
+pop<-as.matrix(counts_numeric2 %>% select(c(54,2,10)) %>% spread(key=ID,value=total_pop))[,-1]
+literate<-as.matrix(counts_numeric2 %>% select(c(54,2,13)) %>% spread(key=ID,value=literate_p))[,-1]
+empty<-as.matrix(counts_numeric2 %>% select(c(54,2,18)) %>% spread(key=ID,value=home_empty_p))[,-1]
+single<-as.matrix(counts_numeric2 %>% select(c(54,2,29)) %>% spread(key=ID,value=single_p))[,-1]
+male<-as.matrix(counts_numeric2 %>% select(c(54,2,19)) %>% spread(key=ID,value=male_p))[,-1]
+unemployed<-as.matrix(counts_numeric2 %>% select(c(54,2,27)) %>% spread(key=ID,value=unem_p))[,-1]
+home<-as.matrix(counts_numeric2 %>% select(c(54,2,28)) %>% spread(key=ID,value=home_p))[,-1]
+
